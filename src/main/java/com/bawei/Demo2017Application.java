cec5bb69eac2457ee3fd077337b1e816eb20ab48
赵德旺 <972525673@qq.com>
2017-07-27 16:12:18 +0800

    111

---
+++ src/main/java/com/bawei/Demo2017Application.java
@@ -0,0 +1,12 @@
+package com.bawei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2017Application.class, args);
+	}
+}
---
+++ src/main/java/com/bawei/Yuekao2Application.java
@@ -0,0 +1,12 @@
+package com.bawei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yuekao2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yuekao2Application.class, args);
+	}
+}
